0823ba803f569d81a3903490da9dbc0eb7181a96
Osamu Noguchi <noguchi@headwaters.co.jp>
2017-04-25 08:21:54 +0900

    add ignore files

--- db/seed.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tasks (
-  id INTEGER PRIMARY KEY,
-  description VARCHAR NOT NULL,
-  completed BOOLEAN NOT NULL DEFAULT 0
-);
-
-INSERT INTO tasks (description) VALUES ("my first task");
